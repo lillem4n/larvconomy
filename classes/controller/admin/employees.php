d1271662929aaad81542cf87f2eed69b74a55ea9
rm <jonas.erlandsson@gmail.com>
2011-11-07 21:53:18 +0100

    Added add user function

--- classes/controller/admin/employees.php
+++ classes/controller/admin/employees.php
@@ -50,12 +50,14 @@ class Controller_Admin_Employees extends Admincontroller {
 		}
 		else
 		{
-			// Add new employee
-//			echo '<pre>';
-//			print_r($_POST);
-//			die();
+     
+      $post = new Validation($_POST);
+      $post->filter('trim');
+      $employee = new Employee();
 
-
+      $employee->set($_POST);
+      $firstname = $post->get('firstname');
+      $this->add_message($firstname . ' was added as employee');
 		}
 	}
 
