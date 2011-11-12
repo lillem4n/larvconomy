1d11f049c7dad61087f63abf8f90fc8d101a407a
Lillem4n <lilleman@larvit.se>
2011-11-12 16:13:46 +0100

    Cleaned up mail class

--- classes/mail.php
+++ classes/mail.php
@@ -2,84 +2,76 @@
 
 class Mail extends Controller {
 
-  private $to = "";
-  private $from = Array();
-  private $subject = "";
-  private $message = "";
-  private $attachment = "";
-  private $attname = "";
+	private $to         = '';
+	private $from       = array();
+	private $subject    = '';
+	private $message    = '';
+	private $attachment = '';
+	private $attname    = '';
 
-  public function __construct()
-  {
-        
-  }
-      
-  public function to($to)
-  {
-    $this->to = $to;
-  }
+	public function to($to)
+	{
+		return ($this->to = $to);
+	}
 
-  public function from($name, $email)
-  {
-    $this->from['name'] = $name;
-    $this->from['email'] = $email;
-  }
+	public function from($name, $email)
+	{
+		$this->from['name']  = $name;
+		$this->from['email'] = $email;
 
-  public function subject($subject) 
-  {
-    $this->subject = $subject;
-  }
+		return TRUE;
+	}
 
-  public function content($message) 
-  {
-    $this->message = $message;
-  }
+	public function subject($subject)
+	{
+		return ($this->subject = $subject);
+	}
 
-  public function attachment($fileURL)
-  {
-    $file = fopen($fileURL, 'rb');
-    $fileContent = stream_get_contents($file);
-    fclose($file);
+	public function content($message)
+	{
+		return ($this->message = $message);
+	}
 
-    $this->attachment = $fileContent;
-    $this->attname = basename($fileURL);
-  }
-  
-  public function send()
-  {
-      $header = 'From: "'.$this->from['name'].'" <'.$this->from['email'].'>\r\n';
-      $header .= 'Reply-To: '.$this->from['email'].'\r\n';
-      $header .= 'Content-Type: text/plain; charset=UTF-8\r\n' .
-                 'Content-Transfer-Encoding: base64\r\n\r\n'; 
+	public function attachment($fileURL)
+	{
+		if ( ! file_exists($fileURL)) return FALSE;
 
-      $content = "";
-      if($this->attachment)
-      {
-        $semi_rand = md5(time());
-        $mime_boundary = '==Multipart_Boundary_x'.$semi_rand.'x';
+		$file             = fopen($fileURL, 'rb');
+		$fileContent      = stream_get_contents($file);
+		fclose($file);
 
-        $header .= '\nMIME-Version: 1.0\n' .
-                    'Content-Type: multipart/mixed;\n' .
-                    'boundary=" '. $mime_boundary .'"';
+		$this->attachment = $fileContent;
+		$this->attname    = basename($fileURL);
 
-        $this->attachment = chunk_split(base64_encode($this->attachment));
-        $content .= '–'.$mime_boundary.'\n' .
-                    'Content-Type: application/pdf;\n' .
-                    'name="'. $this->attname  .'"\n' .
-                    'Content-Disposition: attachment;\n' .
-                    'filename= "'. $this->attname .'"\n' .
-                    'Content-Transfer-Encoding: base64\n\n' .
-                    $this->attachment . '\n\n' .
-                    '-'.$mime_boundary.'-\n';
-      }
-      if( mail($this->to,$this->subject, $this->message, $header) )
-      {
-        return TRUE; 
-      }
-      else
-      {
-        return FALSE;
-      }
-  }
+		return TRUE;
+	}
+
+	public function send()
+	{
+		$header  = 'From: "'.$this->from['name'].'" <'.$this->from['email'].'>\r\n';
+		$header .= 'Reply-To: '.$this->from['email'].'\r\n';
+		$header .= 'Content-Type: text/plain; charset=UTF-8\r\nContent-Transfer-Encoding: base64\r\n\r\n';
+
+		$content = '';
+		if ($this->attachment)
+		{
+			$semi_rand = md5(time());
+			$mime_boundary = '==Multipart_Boundary_x'.$semi_rand.'x';
+
+			$header .= '\nMIME-Version: 1.0\nContent-Type: multipart/mixed;\nboundary=" '.$mime_boundary.'"';
+
+			$this->attachment = chunk_split(base64_encode($this->attachment));
+			$content .= '–'.$mime_boundary.'\n'.
+									'Content-Type: application/pdf;\n'.
+									'name="'.$this->attname.'"\n'.
+									'Content-Disposition: attachment;\n'.
+									'filename= "'.$this->attname.'"\n'.
+									'Content-Transfer-Encoding: base64\n\n'.
+									$this->attachment.'\n\n'.
+									'-'.$mime_boundary.'-\n';
+		}
+
+		return (mail($this->to, $this->subject, $this->message, $header));
+	}
 
 }
